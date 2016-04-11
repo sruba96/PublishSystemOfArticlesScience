4f82123a4f9f4af2254ce405868b40f2f4ec2c91
Paweł Malina <sruba96@gmail.com>
2016-04-11 17:16:37 +0200

    Something work something not.

--- src/main/java/ie/project/controllers/HomeController.java
+++ src/main/java/ie/project/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package ie.project.controllers;
 
-import ie.project.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    @Autowired
-    UserRepository userRepository;
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(){
+    public String index() {
 
         return "index";
     }
