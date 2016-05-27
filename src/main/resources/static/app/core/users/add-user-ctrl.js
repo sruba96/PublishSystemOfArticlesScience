22d9c2fc768fc894460fd5fcf2ce85abb0b9fd59
sruba96 <sruba96@gmail.com>
2016-05-27 14:31:31 +0200

    Checking email and login is finished.

--- src/main/resources/static/app/core/users/add-user-ctrl.js
+++ src/main/resources/static/app/core/users/add-user-ctrl.js
@@ -3,24 +3,15 @@
  */
 app.controller('addUserCtrl', function ($scope, $http) // serwis $http umożliwia zapytania ajax
 {
-
-
-
     this.user = {};
 
     $scope.addUser = function () {
 
-        // this.user = {};
-        console.log(this.user);
-        console.log($scope.user);
-
         var ajax = $http.post('addUser', this.user);
         
-        console.log("function is working");
-
         ajax.success(function (data) {
             if (data.result) {
-                console.log("Response is working");
+                
 
             }
         });
