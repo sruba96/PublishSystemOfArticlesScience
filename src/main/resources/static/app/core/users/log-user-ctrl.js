f6435918c1b622613ab5715f753dfa2d823ebf15
sruba96 <sruba96@gmail.com>
2016-05-30 22:12:00 +0200

    Done - i need new functionality

--- src/main/resources/static/app/core/users/log-user-ctrl.js
+++ src/main/resources/static/app/core/users/log-user-ctrl.js
@@ -1,18 +1,56 @@
 /**
  * Created by pawel on 22.05.16.
  */
-app.controller('logUserCtrl', function ($scope, $http)
-{
-    this.user = {};
-    $scope.logUser = function () {
+(function () {
+    "use strict";
+    angular.module("app")
+        .controller('logUserCtrl', function ($scope, $http) {
+            this.user = {};
+            $scope.logUser = function () {
 
-        console.log(this.user);
-        var ajax = $http.post('logging', this.user);
+                console.log(this.user);
+                var ajax = $http.post('logging', this.user);
 
-        ajax.success(function (data) {
-            if (data.result) {
-                console.log("good you are log in");
-            }
+                ajax.success(function (data) {
+                    if (data.result) {
+                        console.log("good you are log in");
+                        location.reload("#/");
+                    }
+
+                    $scope.checkSession();
+                    console.log(self);
+                });
+
+
+            };
+
+            // other controller
+            $scope.invisible = false;
+            $scope.invisibleLog = true;
+
+
+            var self = $scope;
+
+            $scope.login = "";
+
+            $scope.checkSession = function () {
+
+                var ajax = $http.post('checkLoggedIn');
+
+                console.log("checking...");
+
+                ajax.success(function (data) {
+                    if (data.result) {
+                        console.log("you are logged :)");
+                        self.login = data.message;
+                        self.logged = true;
+                        self.invisible = true;
+                        self.invisibleLog = false;
+
+                    }
+                });
+
+            };
+            $scope.checkSession();
         });
-    };
-});
+})();
