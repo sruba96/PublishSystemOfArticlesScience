3d8a9b1b578469444bf6e207eaadd2b8cf0aafd9
sruba96 <sruba96@gmail.com>
2016-06-08 23:56:46 +0200

    Something good

---
+++ src/main/java/ie/project/repositories/UserRepository2.java
@@ -0,0 +1,11 @@
+package ie.project.repositories;
+
+import ie.project.domain.User;
+import org.springframework.data.repository.Repository;
+
+/**
+ * Created by pawel on 08.06.16.
+ */
+public interface UserRepository2 extends Repository<User, Long>{
+
+}
---
+++ src/main/java/ie/project/repositories/UserRepostiory2Impl.java
@@ -0,0 +1,11 @@
+package ie.project.repositories;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by pawel on 08.06.16.
+ */
+@Repository
+public class UserRepostiory2Impl implements UserRepository2 {
+
+}
